360d117efc76b92dfe89e5d3869819bfee662cde
richard fontaine <ririfonfon10@gmail.com>
2020-11-06 23:00:32 +0100

    new grp

--- Console_DMX_html_VS_1_esp32/src/fonction_srv_handle_set.h
+++ Console_DMX_html_VS_1_esp32/src/fonction_srv_handle_set.h
@@ -26,69 +26,130 @@ void srv_handle_set()
 
             if (Mem == 1)
             {
+                //grp rgb Led
                 lred = 255;
                 lgreen = 0;
                 lblue = 255;
-                //grp rgb comptoire gauche
-                cgred = lred;
-                cggreen = lgreen;
-                cgblue = lblue;
-                //grp rgb comptoire centrale
+
+                //grp rgb Transbo 1
+                tred = lred;
+                tgreen = lgreen;
+                tblue = lblue;
+
+                //grp rgbw Transbo 2
+                ttred = lred;
+                ttgreen = lgreen;
+                ttblue = lblue;
+                ttwhite = 0;
+
+                //grp rgb comptoire
                 cred = lred;
                 cgreen = lgreen;
                 cblue = lblue;
-                //grp rgb comptoire droit
-                cdred = lred;
-                cdgreen = lgreen;
-                cdblue = lblue;
 
-                send_rvb();
-                send_rvb2();
-            } //mem1 mauve
+                //grp rgbw logo
+                lored = lred;
+                logreen = lgreen;
+                loblue = lblue;
+                lowhite = 0;
+
+                //grp rgbw club
+                clured = lred;
+                clugreen = lgreen;
+                clublue = lblue;
+                cluwhite = 0;
+
+                //grp rgb regie
+                rered = lred;
+                regreen = lgreen;
+                reblue = lblue;
+
+                send_rvb6(); // all
+            }                //mem1 mauve
 
             if (Mem == 2)
             {
                 lred = 255;
                 lgreen = 0;
                 lblue = 0;
-                //grp rgb comptoire gauche
-                cgred = lred;
-                cggreen = lgreen;
-                cgblue = lblue;
-                //grp rgb comptoire centrale
+
+                //grp rgb Transbo 1
+                tred = lred;
+                tgreen = lgreen;
+                tblue = lblue;
+
+                //grp rgbw Transbo 2
+                ttred = lred;
+                ttgreen = lgreen;
+                ttblue = lblue;
+                ttwhite = 0;
+
+                //grp rgb comptoire
                 cred = lred;
                 cgreen = lgreen;
                 cblue = lblue;
-                //grp rgb comptoire droit
-                cdred = lred;
-                cdgreen = lgreen;
-                cdblue = lblue;
 
-                send_rvb();
-                send_rvb2();
-            } //mem2 rouge
+                //grp rgbw logo
+                lored = lred;
+                logreen = lgreen;
+                loblue = lblue;
+                lowhite = 0;
+
+                //grp rgbw club
+                clured = lred;
+                clugreen = lgreen;
+                clublue = lblue;
+                cluwhite = 0;
+
+                //grp rgb regie
+                rered = lred;
+                regreen = lgreen;
+                reblue = lblue;
+
+                send_rvb6(); // all
+            }                //mem2 rouge
 
             if (Mem == 3)
             {
                 lred = 0;
                 lgreen = 255;
                 lblue = 0;
-                //grp rgb comptoire gauche
-                cgred = lred;
-                cggreen = lgreen;
-                cgblue = lblue;
-                //grp rgb comptoire centrale
+
+                //grp rgb Transbo 1
+                tred = lred;
+                tgreen = lgreen;
+                tblue = lblue;
+
+                //grp rgbw Transbo 2
+                ttred = lred;
+                ttgreen = lgreen;
+                ttblue = lblue;
+                ttwhite = 0;
+
+                //grp rgb comptoire
                 cred = lred;
                 cgreen = lgreen;
                 cblue = lblue;
-                //grp rgb comptoire droit
-                cdred = lred;
-                cdgreen = lgreen;
-                cdblue = lblue;
 
-                send_rvb();
-                send_rvb2();
-            } //mem3 vert
+                //grp rgbw logo
+                lored = lred;
+                logreen = lgreen;
+                loblue = lblue;
+                lowhite = 0;
+
+                //grp rgbw club
+                clured = lred;
+                clugreen = lgreen;
+                clublue = lblue;
+                cluwhite = 0;
+
+                //grp rgb regie
+                rered = lred;
+                regreen = lgreen;
+                reblue = lblue;
+
+                send_rvb6(); // all
+            }                //mem3 vert
 
             if (Mem == 4)
             {
@@ -96,22 +157,41 @@ void srv_handle_set()
                 lgreen = 0;
                 lblue = 255;
 
-                //grp rgb comptoire gauche
-                cgred = lred;
-                cggreen = lgreen;
-                cgblue = lblue;
-                //grp rgb comptoire centrale
+                //grp rgb Transbo 1
+                tred = lred;
+                tgreen = lgreen;
+                tblue = lblue;
+
+                //grp rgbw Transbo 2
+                ttred = lred;
+                ttgreen = lgreen;
+                ttblue = lblue;
+                ttwhite = 0;
+
+                //grp rgb comptoire
                 cred = lred;
                 cgreen = lgreen;
                 cblue = lblue;
-                //grp rgb comptoire droit
-                cdred = lred;
-                cdgreen = lgreen;
-                cdblue = lblue;
 
-                send_rvb();
-                send_rvb2();
-            } //mem4 bleu
+                //grp rgbw logo
+                lored = lred;
+                logreen = lgreen;
+                loblue = lblue;
+                lowhite = 0;
+
+                //grp rgbw club
+                clured = lred;
+                clugreen = lgreen;
+                clublue = lblue;
+                cluwhite = 0;
+
+                //grp rgb regie
+                rered = lred;
+                regreen = lgreen;
+                reblue = lblue;
+
+                send_rvb6(); // all
+            }                //mem4 bleu
 
             if (Mem == 5)
             {
@@ -119,60 +199,100 @@ void srv_handle_set()
                 lgreen = 127;
                 lblue = 0;
 
-                //grp rgb comptoire gauche
-                cgred = lred;
-                cggreen = lgreen;
-                cgblue = lblue;
-                //grp rgb comptoire centrale
+                //grp rgb Transbo 1
+                tred = lred;
+                tgreen = lgreen;
+                tblue = lblue;
+
+                //grp rgbw Transbo 2
+                ttred = lred;
+                ttgreen = lgreen;
+                ttblue = lblue;
+                ttwhite = 0;
+
+                //grp rgb comptoire
                 cred = lred;
                 cgreen = lgreen;
                 cblue = lblue;
-                //grp rgb comptoire droit
-                cdred = lred;
-                cdgreen = lgreen;
-                cdblue = lblue;
 
-                send_rvb();
-                send_rvb2();
-            } //mem5 orange
+                //grp rgbw logo
+                lored = lred;
+                logreen = lgreen;
+                loblue = lblue;
+                lowhite = 0;
+
+                //grp rgbw club
+                clured = lred;
+                clugreen = lgreen;
+                clublue = lblue;
+                cluwhite = 0;
+
+                //grp rgb regie
+                rered = lred;
+                regreen = lgreen;
+                reblue = lblue;
+
+                send_rvb6(); // all
+            }                //mem5 orange
 
             if (Mem == 6)
             {
-                ccgred = EEPROM.read(1);
-                ccggreen = EEPROM.read(2);
-                ccgblue = EEPROM.read(3);
-                ccred = EEPROM.read(4);
-                ccgreen = EEPROM.read(5);
-                ccblue = EEPROM.read(6);
-                ccdred = EEPROM.read(7);
-                ccdgreen = EEPROM.read(8);
-                ccdblue = EEPROM.read(9);
-                ctred = EEPROM.read(10);
-                ctgreen = EEPROM.read(11);
-                ctblue = EEPROM.read(12);
-                clred = EEPROM.read(13);
-                clgreen = EEPROM.read(14);
-                clblue = EEPROM.read(15);
-                //grp rgb comptoire gauche
-                cgred = ccgred;
-                cggreen = ccggreen;
-                cgblue = ccgblue;
-                //grp rgb comptoire centrale
+                ccred = EEPROM.read(1);
+                ccgreen = EEPROM.read(2);
+                ccblue = EEPROM.read(3);
+                clured = EEPROM.read(4);
+                clgreen = EEPROM.read(5);
+                clblue = EEPROM.read(6);
+                ctred = EEPROM.read(7);
+                ctgreen = EEPROM.read(8);
+                ctblue = EEPROM.read(9);
+                cttred = EEPROM.read(10);
+                cttgreen = EEPROM.read(11);
+                cttblue = EEPROM.read(12);
+                cttwhite = EEPROM.read(13);
+                clored = EEPROM.read(14);
+                clogreen = EEPROM.read(15);
+                cloblue = EEPROM.read(16);
+                clowhite = EEPROM.read(17);
+                cclured = EEPROM.read(18);
+                cclugreen = EEPROM.read(19);
+                cclublue = EEPROM.read(20);
+                ccluwhite = EEPROM.read(21);
+                crered = EEPROM.read(22);
+                cregreen = EEPROM.read(23);
+                creblue = EEPROM.read(24);
+
+                //grp rgb comptoire
                 cred = ccred;
                 cgreen = ccgreen;
                 cblue = ccblue;
-                //grp rgb comptoire droit
-                cdred = ccdred;
-                cdgreen = ccdgreen;
-                cdblue = ccdblue;
+
+                lred = clured;
+                lgreen = clgreen;
+                lblue = clblue;
 
                 tred = ctred;
                 tgreen = ctgreen;
                 tblue = ctblue;
 
-                lred = clred;
-                lgreen = clgreen;
-                lblue = clblue;
+                ttred = cttred;
+                ttgreen = cttgreen;
+                ttblue = cttblue;
+                ttwhite = cttwhite;
+
+                lored = clored;
+                logreen = clogreen;
+                loblue = cloblue;
+                lowhite = clowhite;
+
+                clured = cclured;
+                clugreen = cclugreen;
+                clublue = cclublue;
+                cluwhite = ccluwhite;
+
+                rered = crered;
+                regreen = cregreen;
+                reblue = creblue;
 
                 send_rvb6();
             } //mem6 special
@@ -219,6 +339,7 @@ void srv_handle_set()
                 save_spec();
             } //if(Mem==1){
         }     //serveur mem
-    }         //serveur args
+
+    } //serveur args
     server.send(200, "text/plain", "OK");
 } //srv_handle_set()
