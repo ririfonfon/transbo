3016548fdd7733a6069e77c39c7971e7ffad8234
richard fontaine <ririfonfon10@gmail.com>
2020-11-06 23:02:56 +0100

    add master 9 & good range

--- Console_DMX_html_VS_1_esp32/src/fonction_web_socket.h
+++ Console_DMX_html_VS_1_esp32/src/fonction_web_socket.h
@@ -3,44 +3,44 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
 {
     switch (type)
     {
-     case WStype_DISCONNECTED:
+    case WStype_DISCONNECTED:
         clientn = clientn - 1;
         list[clientn] = 0;
-     #ifdef DEBUG
+#ifdef DEBUG
         Serial.println("Disconnected!");
         Serial.print("clientn : ");
         Serial.println(clientn);
         Serial.print("list[clientn] : ");
         Serial.println(list[clientn]);
-     #endif
+#endif
 
         break;
 
-     case WStype_CONNECTED:
+    case WStype_CONNECTED:
         clientn = clientn + 1;
         list[clientn] = num;
-     #ifdef DEBUG
+#ifdef DEBUG
         Serial.println("Client connected!");
         Serial.print("clientn : ");
         Serial.println(clientn);
         Serial.print("list[clientn] : ");
         Serial.println(list[clientn]);
-     #endif
+#endif
 
         break;
 
-     case WStype_TEXT:
+    case WStype_TEXT:
         //  is the start for this data
         if (payload[0] == 'a')
         {
-         #ifdef DEBUGSPEC
+#ifdef DEBUGSPEC
             Serial.println("payload[0] == 'a'");
-         #endif
+#endif
             if (payload[1] == 'a')
             {
-             #ifdef DEBUGSPEC
+#ifdef DEBUGSPEC
                 Serial.println("payload[1] == 'a'");
-             #endif
+#endif
                 char *pEnd;
                 Mast[1] = strtol((const char *)&payload[2], &pEnd, 8);
                 for (int i = 1; i < (sizeof(M1) / 4); i++)
@@ -71,7 +71,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
                 Mast[3] = strtol((const char *)&payload[2], &pEnd, 8);
                 send_rvb2();
             } //c
-            
+
             if (payload[1] == 'd')
             {
                 char *pEnd;
@@ -119,95 +119,155 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
             {
                 char *pEnd;
                 Mast[8] = strtol((const char *)&payload[2], &pEnd, 8);
-                for (int i = 1; i < (sizeof(M7) / 4); i++)
+                for (int i = 1; i < (sizeof(M8) / 4); i++)
                 {
                     int temp = M8[i];
                     D[temp] = Mast[8];
                     ESP32DMX.setSlot(temp, D[temp]);
-                } //for M7
+                } //for M8
             }     //h
 
-            //  wvu tsr qpo nml kji //
             if (payload[1] == 'i')
             {
                 char *pEnd;
-                ccgred = strtol((const char *)&payload[2], &pEnd, 8);
-            } //i
-            if (payload[1] == 'j')
-            {
-                char *pEnd;
-                ccggreen = strtol((const char *)&payload[2], &pEnd, 8);
-            } //j
-            if (payload[1] == 'k')
-            {
-                char *pEnd;
-                ccgblue = strtol((const char *)&payload[2], &pEnd, 8);
-            } //k
+                Mast[9] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M9) / 4); i++)
+                {
+                    int temp = M9[i];
+                    D[temp] = Mast[9];
+                    ESP32DMX.setSlot(temp, D[temp]);
+                } //for M9
+            }     //i
+        }         //payload[0] = A
 
-            if (payload[1] == 'l')
+        else if (payload[0] == 'b')
+        {
+#ifdef DEBUGSPEC
+            Serial.println("payload[0] == 'b'");
+#endif
+            //  wvu tsr qpo nml kji //
+            if (payload[1] == 'a')
             {
                 char *pEnd;
                 ccred = strtol((const char *)&payload[2], &pEnd, 8);
-            } //l
-            if (payload[1] == 'm')
+            } //a
+            else if (payload[1] == 'b')
             {
                 char *pEnd;
                 ccgreen = strtol((const char *)&payload[2], &pEnd, 8);
-            } //m
-            if (payload[1] == 'n')
+            } //b
+            else if (payload[1] == 'c')
             {
                 char *pEnd;
                 ccblue = strtol((const char *)&payload[2], &pEnd, 8);
-            } //n
-
-            if (payload[1] == 'o')
-            {
-                char *pEnd;
-                ccdred = strtol((const char *)&payload[2], &pEnd, 8);
-            } //o
-            if (payload[1] == 'p')
-            {
-                char *pEnd;
-                ccdgreen = strtol((const char *)&payload[2], &pEnd, 8);
-            } //p
-            if (payload[1] == 'q')
-            {
-                char *pEnd;
-                ccdblue = strtol((const char *)&payload[2], &pEnd, 8);
-            } //q
-
-            if (payload[1] == 'r')
-            {
-                char *pEnd;
-                ctred = strtol((const char *)&payload[2], &pEnd, 8);
-            } //r
-            if (payload[1] == 's')
-            {
-                char *pEnd;
-                ctgreen = strtol((const char *)&payload[2], &pEnd, 8);
-            } //s
-            if (payload[1] == 't')
-            {
-                char *pEnd;
-                ctblue = strtol((const char *)&payload[2], &pEnd, 8);
-            } //t
-
-            if (payload[1] == 'u')
+            } //c
+            else if (payload[1] == 'd')
             {
                 char *pEnd;
                 clred = strtol((const char *)&payload[2], &pEnd, 8);
-            } //u
-            if (payload[1] == 'v')
+            } //d
+            else if (payload[1] == 'e')
             {
                 char *pEnd;
                 clgreen = strtol((const char *)&payload[2], &pEnd, 8);
-            } //v
-            if (payload[1] == 'w')
+            } //e
+            else if (payload[1] == 'f')
             {
                 char *pEnd;
                 clblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //f
+            else if (payload[1] == 'j')
+            {
+                char *pEnd;
+                ctred = strtol((const char *)&payload[2], &pEnd, 8);
+            } //j
+            else if (payload[1] == 'h')
+            {
+                char *pEnd;
+                ctgreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //h
+            else if (payload[1] == 'i')
+            {
+                char *pEnd;
+                ctblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //i
+            else if (payload[1] == 'j')
+            {
+                char *pEnd;
+                cttred = strtol((const char *)&payload[2], &pEnd, 8);
+            } //j
+            else if (payload[1] == 'k')
+            {
+                char *pEnd;
+                cttgreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //k
+            else if (payload[1] == 'l')
+            {
+                char *pEnd;
+                cttblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //l
+            else if (payload[1] == 'm')
+            {
+                char *pEnd;
+                cttwhite = strtol((const char *)&payload[2], &pEnd, 8);
+            } //m
+            else if (payload[1] == 'n')
+            {
+                char *pEnd;
+                clored = strtol((const char *)&payload[2], &pEnd, 8);
+            } //n
+            else if (payload[1] == 'o')
+            {
+                char *pEnd;
+                clogreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //o
+            else if (payload[1] == 'p')
+            {
+                char *pEnd;
+                cloblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //p
+            else if (payload[1] == 'q')
+            {
+                char *pEnd;
+                clowhite = strtol((const char *)&payload[2], &pEnd, 8);
+            } //q
+            else if (payload[1] == 'r')
+            {
+                char *pEnd;
+                cclured = strtol((const char *)&payload[2], &pEnd, 8);
+            } //r
+            else if (payload[1] == 's')
+            {
+                char *pEnd;
+                cclugreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //s
+            else if (payload[1] == 't')
+            {
+                char *pEnd;
+                cclublue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //t
+            else if (payload[1] == 'u')
+            {
+                char *pEnd;
+                ccluwhite = strtol((const char *)&payload[2], &pEnd, 8);
+            } //u
+            else if (payload[1] == 'v')
+            {
+                char *pEnd;
+                crered = strtol((const char *)&payload[2], &pEnd, 8);
+            } //v
+            else if (payload[1] == 'w')
+            {
+                char *pEnd;
+                cregreen = strtol((const char *)&payload[2], &pEnd, 8);
             } //w
-        } //payload[0]
+            else if (payload[1] == 'x')
+            {
+                char *pEnd;
+                creblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //x
+
+        } //payload[0] == B
         break;
     } //type
 } //web socket
