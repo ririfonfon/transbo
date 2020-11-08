fd536dcb333c2121fff9dd56c7a25b0f3a67a8d8
richard fontaine <ririfonfon10@gmail.com>
2020-11-08 10:11:57 +0100

    create v2

---
+++ Console_DMX_html_VS_2_esp32/src/fonction_void.h
@@ -0,0 +1,341 @@
+//////////////////////////////////////////V O I D////////////////////////////////////////
+void load_spec()
+{
+
+    ccred = EEPROM.read(1);
+    ccgreen = EEPROM.read(2);
+    ccblue = EEPROM.read(3);
+    clured = EEPROM.read(4);
+    clgreen = EEPROM.read(5);
+    clblue = EEPROM.read(6);
+    ctred = EEPROM.read(7);
+    ctgreen = EEPROM.read(8);
+    ctblue = EEPROM.read(9);
+    cttred = EEPROM.read(10);
+    cttgreen = EEPROM.read(11);
+    cttblue = EEPROM.read(12);
+    cttwhite = EEPROM.read(13);
+    clored = EEPROM.read(14);
+    clogreen = EEPROM.read(15);
+    cloblue = EEPROM.read(16);
+    clowhite = EEPROM.read(17);
+    cclured = EEPROM.read(18);
+    cclugreen = EEPROM.read(19);
+    cclublue = EEPROM.read(20);
+    ccluwhite = EEPROM.read(21);
+    crered = EEPROM.read(22);
+    cregreen = EEPROM.read(23);
+    creblue = EEPROM.read(24);
+
+    for (int i = 1; i <= clientn; i++)
+    {
+
+        if (list[i] != 0)
+        {
+
+            webSocket.sendTXT(list[i], "ba:" + String(lround(ccred)));
+            webSocket.sendTXT(list[i], "bb:" + String(lround(ccgreen)));
+            webSocket.sendTXT(list[i], "bc:" + String(lround(ccblue)));
+
+            webSocket.sendTXT(list[i], "bd:" + String(lround(clured)));
+            webSocket.sendTXT(list[i], "be:" + String(lround(clgreen)));
+            webSocket.sendTXT(list[i], "bf:" + String(lround(clblue)));
+
+            webSocket.sendTXT(list[i], "bg:" + String(lround(ctred)));
+            webSocket.sendTXT(list[i], "bh:" + String(lround(ctgreen)));
+            webSocket.sendTXT(list[i], "bi:" + String(lround(ctblue)));
+
+            webSocket.sendTXT(list[i], "bj:" + String(lround(cttred)));
+            webSocket.sendTXT(list[i], "bk:" + String(lround(cttgreen)));
+            webSocket.sendTXT(list[i], "bl:" + String(lround(cttblue)));
+            webSocket.sendTXT(list[i], "bm:" + String(lround(cttwhite)));
+
+            webSocket.sendTXT(list[i], "bn:" + String(lround(clored)));
+            webSocket.sendTXT(list[i], "bo:" + String(lround(clogreen)));
+            webSocket.sendTXT(list[i], "bp:" + String(lround(cloblue)));
+            webSocket.sendTXT(list[i], "bq:" + String(lround(clowhite)));
+
+            webSocket.sendTXT(list[i], "br:" + String(lround(cclured)));
+            webSocket.sendTXT(list[i], "bs:" + String(lround(cclugreen)));
+            webSocket.sendTXT(list[i], "bt:" + String(lround(cclublue)));
+            webSocket.sendTXT(list[i], "bu:" + String(lround(ccluwhite)));
+
+            webSocket.sendTXT(list[i], "bv:" + String(lround(crered)));
+            webSocket.sendTXT(list[i], "bw:" + String(lround(cregreen)));
+            webSocket.sendTXT(list[i], "bx:" + String(lround(creblue)));
+
+        } // if (list[i] !=0)
+
+    } // for client
+
+} //load_spec
+
+void save_spec()
+{
+    EEPROM.write(1, ccred);
+    EEPROM.write(2, ccgreen);
+    EEPROM.write(3, ccblue);
+    EEPROM.write(4, clred);
+    EEPROM.write(5, clgreen);
+    EEPROM.write(6, clblue);
+    EEPROM.write(7, ctred);
+    EEPROM.write(8, ctgreen);
+    EEPROM.write(9, ctblue);
+    EEPROM.write(10, cttred);
+    EEPROM.write(11, cttgreen);
+    EEPROM.write(12, cttblue);
+    EEPROM.write(13, cttwhite);
+    EEPROM.write(14, clored);
+    EEPROM.write(15, clogreen);
+    EEPROM.write(16, cloblue);
+    EEPROM.write(17, clowhite);
+    EEPROM.write(18, cclured);
+    EEPROM.write(19, cclugreen);
+    EEPROM.write(20, cclublue);
+    EEPROM.write(21, ccluwhite);
+    EEPROM.write(22, crered);
+    EEPROM.write(23, cregreen);
+    EEPROM.write(24, creblue);
+
+    EEPROM.write(62, 'O');
+    EEPROM.write(63, 'K');
+    EEPROM.commit();
+} //save_spec
+
+void send_rvb()
+{
+    //grp Led
+    for (int i = 1; i < (sizeof(R) / 4); i++)
+    {
+        int temp = R[i];
+        D[temp] = ((lred / 255) * Mast[5]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for R
+
+    for (int i = 1; i < (sizeof(G) / 4); i++)
+    {
+        int temp = G[i];
+        D[temp] = ((lgreen / 255) * Mast[5]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for G
+
+    for (int i = 1; i < (sizeof(B) / 4); i++)
+    {
+        int temp = B[i];
+        D[temp] = ((lblue / 255) * Mast[5]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for B
+
+} //send_rvb
+
+void send_rvb1()
+{
+    //grp Trans 1
+    for (int i = 1; i < (sizeof(RR) / 4); i++)
+    {
+        int temp = RR[i];
+        D[temp] = tred;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for RR
+
+    for (int i = 1; i < (sizeof(GG) / 4); i++)
+    {
+        int temp = GG[i];
+        D[temp] = tgreen;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for GG
+
+    for (int i = 1; i < (sizeof(BB) / 4); i++)
+    {
+        int temp = BB[i];
+        D[temp] = tblue;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for BB
+
+} //send_rvb1
+
+void send_rvb2()
+{
+    //grp Comptoire
+    for (int i = 1; i < (sizeof(RRR) / 4); i++)
+    {
+        int temp = RRR[i];
+        D[temp] = round((cred / 255) * Mast[3]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for RRR
+
+    for (int i = 1; i < (sizeof(GGG) / 4); i++)
+    {
+        int temp = GGG[i];
+        D[temp] = round((cgreen / 255) * Mast[3]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for GGG
+
+    for (int i = 1; i < (sizeof(BBB) / 4); i++)
+    {
+        int temp = BBB[i];
+        D[temp] = round((cblue / 255) * Mast[3]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for BBB
+
+} //send_rvb2
+
+void send_rvb6()
+{
+    ///////////////////////////////////////////////////////grp led
+    for (int i = 1; i < (sizeof(R) / 4); i++)
+    {
+        int temp = R[i];
+        D[temp] = ((lred / 255) * Mast[5]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for R
+
+    for (int i = 1; i < (sizeof(G) / 4); i++)
+    {
+        int temp = G[i];
+        D[temp] = ((lgreen / 255) * Mast[5]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for G
+
+    for (int i = 1; i < (sizeof(B) / 4); i++)
+    {
+        int temp = B[i];
+        D[temp] = ((lblue / 255) * Mast[5]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for B
+
+    ///////////////////////////////////////////////////////grp transbo
+    for (int i = 1; i < (sizeof(RR) / 4); i++)
+    {
+        int temp = RR[i];
+        D[temp] = tred;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for RR
+
+    for (int i = 1; i < (sizeof(GG) / 4); i++)
+    {
+        int temp = GG[i];
+        D[temp] = tgreen;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for GG
+
+    for (int i = 1; i < (sizeof(BB) / 4); i++)
+    {
+        int temp = BB[i];
+        D[temp] = tblue;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for BB
+
+    ///////////////////////////////////////////////////////grp comptoire
+    for (int i = 1; i < (sizeof(RRR) / 4); i++)
+    {
+        int temp = RRR[i];
+        D[temp] = round((cred / 255) * Mast[3]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for RRR
+
+    for (int i = 1; i < (sizeof(GGG) / 4); i++)
+    {
+        int temp = GGG[i];
+        D[temp] = round((cgreen / 255) * Mast[3]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for GGG
+
+    for (int i = 1; i < (sizeof(BBB) / 4); i++)
+    {
+        int temp = BBB[i];
+        D[temp] = round((cblue / 255) * Mast[3]);
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for BBB
+
+    ///////////////////////////////////////////////////////grp trans 2
+    for (int i = 1; i < (sizeof(RRRR) / 4); i++)
+    {
+        int temp = RRRR[i];
+        D[temp] = ttred;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for RR
+
+    for (int i = 1; i < (sizeof(GGGG) / 4); i++)
+    {
+        int temp = GGGG[i];
+        D[temp] = ttgreen;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for GG
+
+    for (int i = 1; i < (sizeof(BBBB) / 4); i++)
+    {
+        int temp = BBBB[i];
+        D[temp] = ttblue;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for BB
+
+    for (int i = 1; i < (sizeof(WWWW) / 4); i++)
+    {
+        int temp = WWWW[i];
+        D[temp] = ttwhite;
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for BB
+
+} //send_rvb6
+
+void send_Mast()
+{
+    for (int i = 1; i < (sizeof(M1) / 4); i++)
+    {
+        int temp = M1[i];
+        D[temp] = Mast[1];
+        ESP32DMX.setSlot(temp, D[temp]);
+
+    } //for M1
+
+    for (int i = 1; i < (sizeof(M2) / 4); i++)
+    {
+        int temp = M2[i];
+        D[temp] = Mast[2];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M2
+
+    for (int i = 1; i < (sizeof(M3) / 4); i++)
+    {
+        int temp = M3[i];
+        D[temp] = Mast[3];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M3
+
+    for (int i = 1; i < (sizeof(M5) / 4); i++)
+    {
+        int temp = M5[i];
+        D[temp] = Mast[5];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M5
+
+    for (int i = 1; i < (sizeof(M6) / 4); i++)
+    {
+        int temp = M6[i];
+        D[temp] = Mast[6];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M6
+
+    for (int i = 1; i < (sizeof(M7) / 4); i++)
+    {
+        int temp = M7[i];
+        D[temp] = Mast[7];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M7
+
+    for (int i = 1; i < (sizeof(M8) / 4); i++)
+    {
+        int temp = M8[i];
+        D[temp] = Mast[8];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M8
+
+    for (int i = 1; i < (sizeof(M9) / 4); i++)
+    {
+        int temp = M9[i];
+        D[temp] = Mast[9];
+        ESP32DMX.setSlot(temp, D[temp]);
+    } //for M9
+
+} //send_Mast
---
+++ Console_DMX_html_VS_2_esp32/src/fonction_web_socket.h
@@ -0,0 +1,286 @@
+//////////////////////////////////////////websocket///////////////////////////////////////////////
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
+{
+    switch (type)
+    {
+    case WStype_DISCONNECTED:
+        clientn = clientn - 1;
+        list[clientn] = 0;
+#ifdef DEBUG
+        Serial.println("Disconnected!");
+        Serial.print("clientn : ");
+        Serial.println(clientn);
+        Serial.print("list[clientn] : ");
+        Serial.println(list[clientn]);
+#endif
+
+        break;
+
+    case WStype_CONNECTED:
+        clientn = clientn + 1;
+        list[clientn] = num;
+#ifdef DEBUG
+        Serial.println("Client connected!");
+        Serial.print("clientn : ");
+        Serial.println(clientn);
+        Serial.print("list[clientn] : ");
+        Serial.println(list[clientn]);
+#endif
+
+        break;
+
+    case WStype_TEXT:
+        //  is the start for this data
+        if (payload[0] == 'a')
+        {
+            for (int i = 0; i < clientn; i++)
+            {
+                webSocket.sendTXT(list[i], payload);
+                #ifdef DEBUG
+                Serial.println("webSocket.sendTXT(list[i], payload);");
+                Serial.print("clientn : ");
+                Serial.println(clientn);
+                Serial.print("list[clientn] : ");
+                Serial.println(list[clientn]);
+                #endif
+
+            }
+
+#ifdef DEBUGSPEC
+            Serial.println("payload[0] == 'a'");
+#endif
+            if (payload[1] == 'a')
+            {
+#ifdef DEBUGSPEC
+                Serial.println("payload[1] == 'a'");
+#endif
+                char *pEnd;
+                Mast[1] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M1) / 4); i++)
+                {
+                    int temp = M1[i];
+                    D[temp] = Mast[1];
+                    ESP32DMX.setSlot(temp, D[temp]);
+
+                } //for M1
+            }     //a
+
+            if (payload[1] == 'b')
+            {
+                char *pEnd;
+                Mast[2] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M2) / 4); i++)
+                {
+                    int temp = M2[i];
+                    D[temp] = Mast[2];
+                    ESP32DMX.setSlot(temp, D[temp]);
+
+                } //for M2
+            }     //b
+
+            if (payload[1] == 'c')
+            {
+                char *pEnd;
+                Mast[3] = strtol((const char *)&payload[2], &pEnd, 8);
+                send_rvb2();
+            } //c
+
+            if (payload[1] == 'd')
+            {
+                char *pEnd;
+                Mast[5] = strtol((const char *)&payload[2], &pEnd, 8);
+                send_rvb();
+            } //d
+
+            if (payload[1] == 'e')
+            {
+                char *pEnd;
+                Mast[5] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M5) / 4); i++)
+                {
+                    int temp = M5[i];
+                    D[temp] = Mast[5];
+                    ESP32DMX.setSlot(temp, D[temp]);
+                } //for M5
+            }     //e
+
+            if (payload[1] == 'f')
+            {
+                char *pEnd;
+                Mast[6] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M6) / 4); i++)
+                {
+                    int temp = M6[i];
+                    D[temp] = Mast[6];
+                    ESP32DMX.setSlot(temp, D[temp]);
+                } //for M6
+            }     //f
+
+            if (payload[1] == 'g')
+            {
+                char *pEnd;
+                Mast[7] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M7) / 4); i++)
+                {
+                    int temp = M7[i];
+                    D[temp] = Mast[7];
+                    ESP32DMX.setSlot(temp, D[temp]);
+                } //for M7
+            }     //g
+
+            if (payload[1] == 'h')
+            {
+                char *pEnd;
+                Mast[8] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M8) / 4); i++)
+                {
+                    int temp = M8[i];
+                    D[temp] = Mast[8];
+                    ESP32DMX.setSlot(temp, D[temp]);
+                } //for M8
+            }     //h
+
+            if (payload[1] == 'i')
+            {
+                char *pEnd;
+                Mast[9] = strtol((const char *)&payload[2], &pEnd, 8);
+                for (int i = 1; i < (sizeof(M9) / 4); i++)
+                {
+                    int temp = M9[i];
+                    D[temp] = Mast[9];
+                    ESP32DMX.setSlot(temp, D[temp]);
+                } //for M9
+            }     //i
+        }         //payload[0] = A
+
+        else if (payload[0] == 'b')
+        {
+#ifdef DEBUGSPEC
+            Serial.println("payload[0] == 'b'");
+#endif
+            //  wvu tsr qpo nml kji //
+            if (payload[1] == 'a')
+            {
+                char *pEnd;
+                ccred = strtol((const char *)&payload[2], &pEnd, 8);
+            } //a
+            else if (payload[1] == 'b')
+            {
+                char *pEnd;
+                ccgreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //b
+            else if (payload[1] == 'c')
+            {
+                char *pEnd;
+                ccblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //c
+            else if (payload[1] == 'd')
+            {
+                char *pEnd;
+                clred = strtol((const char *)&payload[2], &pEnd, 8);
+            } //d
+            else if (payload[1] == 'e')
+            {
+                char *pEnd;
+                clgreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //e
+            else if (payload[1] == 'f')
+            {
+                char *pEnd;
+                clblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //f
+            else if (payload[1] == 'j')
+            {
+                char *pEnd;
+                ctred = strtol((const char *)&payload[2], &pEnd, 8);
+            } //j
+            else if (payload[1] == 'h')
+            {
+                char *pEnd;
+                ctgreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //h
+            else if (payload[1] == 'i')
+            {
+                char *pEnd;
+                ctblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //i
+            else if (payload[1] == 'j')
+            {
+                char *pEnd;
+                cttred = strtol((const char *)&payload[2], &pEnd, 8);
+            } //j
+            else if (payload[1] == 'k')
+            {
+                char *pEnd;
+                cttgreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //k
+            else if (payload[1] == 'l')
+            {
+                char *pEnd;
+                cttblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //l
+            else if (payload[1] == 'm')
+            {
+                char *pEnd;
+                cttwhite = strtol((const char *)&payload[2], &pEnd, 8);
+            } //m
+            else if (payload[1] == 'n')
+            {
+                char *pEnd;
+                clored = strtol((const char *)&payload[2], &pEnd, 8);
+            } //n
+            else if (payload[1] == 'o')
+            {
+                char *pEnd;
+                clogreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //o
+            else if (payload[1] == 'p')
+            {
+                char *pEnd;
+                cloblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //p
+            else if (payload[1] == 'q')
+            {
+                char *pEnd;
+                clowhite = strtol((const char *)&payload[2], &pEnd, 8);
+            } //q
+            else if (payload[1] == 'r')
+            {
+                char *pEnd;
+                cclured = strtol((const char *)&payload[2], &pEnd, 8);
+            } //r
+            else if (payload[1] == 's')
+            {
+                char *pEnd;
+                cclugreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //s
+            else if (payload[1] == 't')
+            {
+                char *pEnd;
+                cclublue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //t
+            else if (payload[1] == 'u')
+            {
+                char *pEnd;
+                ccluwhite = strtol((const char *)&payload[2], &pEnd, 8);
+            } //u
+            else if (payload[1] == 'v')
+            {
+                char *pEnd;
+                crered = strtol((const char *)&payload[2], &pEnd, 8);
+            } //v
+            else if (payload[1] == 'w')
+            {
+                char *pEnd;
+                cregreen = strtol((const char *)&payload[2], &pEnd, 8);
+            } //w
+            else if (payload[1] == 'x')
+            {
+                char *pEnd;
+                creblue = strtol((const char *)&payload[2], &pEnd, 8);
+            } //x
+
+        } //payload[0] == B
+        break;
+    } //type
+} //web socket
