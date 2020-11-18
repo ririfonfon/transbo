490ac312b5e3f285123974aa905828ead0f2fbd0
richard fontaine <ririfonfon10@gmail.com>
2020-11-18 16:55:56 +0100

    Update wifi_variable.h

--- Console_DMX_html_VS_1_esp32/src/wifi_variable.h
+++ Console_DMX_html_VS_1_esp32/src/wifi_variable.h
@@ -2,7 +2,10 @@ const char *host = "console";
 
 // WiFi network settings
 
-const char *ssid = "esptransbo";
-const char *password = "esptransbo";
+// const char *ssid = "esptransbo";
+// const char *password = "esptransbo";
 
-#define MAX_CLIENT 8
+const char *ssid = "riri_new";
+const char *password = "B2az41opbn6397";
+
+#define MAX_CLIENT 6
