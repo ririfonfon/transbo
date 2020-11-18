c8bd66f390d2cd2dbff12d3a4d124a345ebd845e
richard fontaine <ririfonfon10@gmail.com>
2020-11-18 16:38:32 +0100

    double load on connect

--- Console_DMX_html_VS_1_esp32/data/main.js
+++ Console_DMX_html_VS_1_esp32/data/main.js
@@ -11,7 +11,7 @@ function connect() {
     connection.onopen = function() {
         connection.send('Connect ' + new Date());
         console.log('connection.send.');
-        connection.send("*LOAD*");
+        // connection.send("*LOAD*");
     };
 
     connection.onmessage = function(e) {
