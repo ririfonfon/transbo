21cb4de3dba7e6c21c292ba6d687b2941872ab74
richard fontaine <ririfonfon10@gmail.com>
2022-05-03 19:59:54 +0200

    add escalier route medza

--- Console_DMX_html_VS_1_esp32/src/a_mem_value.h
+++ Console_DMX_html_VS_1_esp32/src/a_mem_value.h
@@ -4,40 +4,43 @@ void mem_value(int a)
 {
     if (a == 1)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 255;
         lgreen = 0;
         lblue = 255;
+        lwhite = 0;
+        lambre = 0;
+        luv = 100;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -51,43 +54,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 2)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 255;
         lgreen = 0;
         lblue = 0;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -101,43 +131,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 3)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 0;
         lgreen = 255;
         lblue = 0;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -151,43 +208,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 4)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 0;
         lgreen = 0;
         lblue = 255;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -201,43 +285,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 5)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 255;
         lgreen = 127;
         lblue = 0;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -251,37 +362,61 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 6)
     {
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = ccred;
         cgreen = ccgreen;
         cblue = ccblue;
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = clured;
         lgreen = clgreen;
         lblue = clblue;
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = ctred;
         tgreen = ctgreen;
         tblue = ctblue;
-        //grp rgb Transbo 2
+        // grp rgb Transbo 2
         ttred = cttred;
         ttgreen = cttgreen;
         ttblue = cttblue;
         ttwhite = cttwhite;
-        //grp rgbw logo
+        // grp rgbw logo
         lored = clored;
         logreen = clogreen;
         loblue = cloblue;
         lowhite = clowhite;
-        //grp rgbw club
+        // grp rgbw club
         clured = cclured;
         clugreen = cclugreen;
         clublue = cclublue;
         cluwhite = ccluwhite;
-        //grp rgb regie
+        // grp rgb regie
         rered = crered;
         regreen = cregreen;
         reblue = creblue;
@@ -295,43 +430,70 @@ void mem_value(int a)
         bred = cbred;
         bgreen = cbgreen;
         bblue = cbblue;
+
+        // grp escalier
+        ered = cered;
+        egreen = cegreen;
+        eblue = ceblue;
+        ewhite = cewhite;
+        eambre = ceambre;
+        euv = ceuv;
+
+        // grp route
+        rored = crored;
+        rogreen = crogreen;
+        roblue = croblue;
+        rowhite = crowhite;
+        roambre = croambre;
+        rouv = crouv;
+
+        // grp medza
+        mered = cmered;
+        megreen = cmegreen;
+        meblue = cmeblue;
+        mewhite = cmewhite;
+        meambre = cmeambre;
+        meuv = cmeuv;
     }
     else if (a == 8)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 255;
         lgreen = 255;
         lblue = 0;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -345,43 +507,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 9)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 255;
         lgreen = 0;
         lblue = 255;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -395,43 +584,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 10)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 0;
         lgreen = 255;
         lblue = 255;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = defowhite;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = defowhite;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -445,43 +661,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 11)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 0;
         lgreen = 0;
         lblue = 0;
+        lwhite = 0;
+        lambre = 0;
+        luv = 0;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = 0;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = 0;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 0;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -495,43 +738,70 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
     else if (a == 12)
     {
-        //grp rgb Led bar
+        // grp rgb Led bar
         lred = 255;
         lgreen = 255;
         lblue = 255;
+        lwhite = 255;
+        lambre = 255;
+        luv = 255;
 
-        //grp rgb Transbo 1
+        // grp rgb Transbo 1
         tred = lred;
         tgreen = lgreen;
         tblue = lblue;
 
-        //grp rgbw Transbo 2
+        // grp rgbw Transbo 2
         ttred = lred;
         ttgreen = lgreen;
         ttblue = lblue;
         ttwhite = 255;
 
-        //grp rgb colonnes
+        // grp rgb colonnes
         cred = lred;
         cgreen = lgreen;
         cblue = lblue;
 
-        //grp rgbw logo
+        // grp rgbw logo
         lored = lred;
         logreen = lgreen;
         loblue = lblue;
         lowhite = 255;
 
-        //grp rgbw club
+        // grp rgbw club
         clured = lred;
         clugreen = lgreen;
         clublue = lblue;
         cluwhite = 255;
 
-        //grp rgb regie
+        // grp rgb regie
         rered = lred;
         regreen = lgreen;
         reblue = lblue;
@@ -545,5 +815,29 @@ void mem_value(int a)
         bred = lred;
         bgreen = lgreen;
         bblue = lblue;
+
+        // grp escalier
+        ered = lred;
+        egreen = lgreen;
+        eblue = lblue;
+        ewhite = 255;
+        eambre = lambre;
+        euv = luv;
+
+        // grp route
+        rored = lred;
+        rogreen = lgreen;
+        roblue = lblue;
+        rowhite = lwhite;
+        roambre = lambre;
+        rouv = luv;
+
+        // grp medza
+        mered = lred;
+        megreen = lgreen;
+        meblue = lblue;
+        mewhite = 255;
+        meambre = lambre;
+        meuv = luv;
     }
 }
