62ea69555f0dc272792a51e6c02cc8a60d8b0550
richard fontaine <ririfonfon10@gmail.com>
2022-10-31 12:39:01 +0100

    old esp

--- Console_DMX_html_VS_1_esp32/src/main.cpp
+++ Console_DMX_html_VS_1_esp32/src/main.cpp
@@ -1,6 +1,6 @@
 // platformio run --target uploadfs // cmd console download spiff
 
-#define VERSION 8
+#define VERSION 9
 
 // #define DEBUG 1
 // #define DEBUGH 1
@@ -92,7 +92,7 @@ void setup()
 
   WiFi.setHostname(host);
   // IPAddress Ip(192, 168, 0, 150);// riri dev
-  IPAddress Ip(192, 168, 2, 250);
+  IPAddress Ip(192, 168, 2, 250);// Transbo
   IPAddress NMask(255, 0, 0, 0);
   WiFi.config(Ip, Ip, NMask);
 
--- Console_DMX_html_VS_1_esp32/src/wifi_variable.h
+++ Console_DMX_html_VS_1_esp32/src/wifi_variable.h
@@ -2,17 +2,9 @@ const char *host = "console";
 
 // WiFi network settings
 
-// const char *ssid = "esptransbo";
-// const char *password = "esptransbo";
-
 // const char *ssid = "riri_new";
 // const char *password = "B2az41opbn6397";
 
-// const char *ssid = "kxkm-wifi";
-// const char *password = "KOMPLEXKAPHARNAUM";
-
-// const char *ssid = "loge transbo";
-// const char *password = "experience";
 
 const char *ssid = "esp-club";
 const char *password = "lightclub";
